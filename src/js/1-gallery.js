eaa2f2e91996a6e764e7446f2b817897d3226b7f
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-02-01 00:24:35 +0200

    fff

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,5 @@
 import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css'; 
+import './node_modules/simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
  {
